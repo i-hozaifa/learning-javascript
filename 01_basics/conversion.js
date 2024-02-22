5dea638bd20d386004e0d19e0f0ec76603ba6af6
Hozaifa <160639647+i-hozaifa@users.noreply.github.com>
2024-02-22 07:30:44 +0000

    datatypees

---
+++ 01_basics/conversion.js
@@ -0,0 +1,9 @@
+ let SomeNUmber = 33
+ let StringNumber = String(SomeNUmber)
+ console.log(StringNumber)
+ console.log(typeof StringNumber);
+
+ let score = "33"
+ let StringToNumber = Number(score)
+ console.log(StringToNumber);
+ console.log(typeof StringToNumber);
