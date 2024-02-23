671e6d854ecc841d8c145c5c63bc9f98f0e22a62
Hozaifa <160639647+i-hozaifa@users.noreply.github.com>
2024-02-23 16:39:36 +0000

    stringss

---
+++ 01_basics/stringss.js
@@ -0,0 +1,12 @@
+let name = "hozaifa"
+let secondName = 'Taqui'
+
+// console.log(name + secondName + " anythings");
+
+console.log(`hello my name is ${name} and my second name is ${secondName} and whatever uh can call`);
+
+const GameName = new String('Hozaifa') // way to declare Strings in JS although it return same value as normal declaration
+console.log(GameName[0]);
+console.log(GameName.__proto__);
+console.log(GameName.length);
+console.log(GameName.toUpperCase());
