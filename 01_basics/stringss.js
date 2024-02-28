e2aa541e7d19df58c4d4b2bd11fa6e7dd0f771f4
Hozaifa <160639647+i-hozaifa@users.noreply.github.com>
2024-02-28 16:09:48 +0000

    heyyy

--- 01_basics/stringss.js
+++ 01_basics/stringss.js
@@ -10,3 +10,17 @@ console.log(GameName[0]);
 console.log(GameName.__proto__);
 console.log(GameName.length);
 console.log(GameName.toUpperCase());
+console.log(GameName.charAt(2));
+console.log(GameName.indexOf('z'));
+const newString = GameName.substring(0,3);
+console.log(newString);
+const neww = GameName.slice(-7,3)   // to make start from reverse
+console.log(neww);
+const word = '    hzz      '
+console.log(word);
+console.log(word.trim());   // to trim extra spacess
+
+const url = 'https://hozaifa.com/hozaifa%20ahmed'
+console.log(url.replace('%20','##'));
+ const sperate = 'hozaifa-hz-taqui'
+ console.log(sperate.split('-'));
