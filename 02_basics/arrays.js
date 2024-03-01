fd4674d66b1dae34bc4ba24a14a64c9c9a015735
Hozaifa <160639647+i-hozaifa@users.noreply.github.com>
2024-03-01 10:51:24 +0000

    new

---
+++ 02_basics/arrays.js
@@ -0,0 +1,11 @@
+const myArr = [1,2,3,4,5,6,7,8]
+const heroes = ['ironman','captain america','hulk']
+const myArr2 = new Array(1,2,34,56,990)
+// these are the ways to declare in array
+
+console.log(myArr2[3]);    // to find value of any index of an arrays
+
+
+// Arrays methods........
+
+ 
